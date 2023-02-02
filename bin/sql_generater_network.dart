6d5b9b57943c8c53ecb60bad0830b42dee5d6b73
jay <yuanjie1224@gmail.com>
2023-02-02 15:21:34 +0800

    a

---
+++ bin/sql_generater_network.dart
@@ -0,0 +1,5 @@
+import 'package:sql_generater_network/sql_generater_network.dart' as sql_generater_network;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sql_generater_network.calculate()}!');
+}
---
+++ test/sql_generater_network_test.dart
@@ -0,0 +1,8 @@
+import 'package:sql_generater_network/sql_generater_network.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
